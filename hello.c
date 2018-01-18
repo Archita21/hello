7acbf4eec0c390168243b86d17e9b9ec0da0d8a3
Archita21 <35433234+Archita21@users.noreply.github.com>
2018-01-19 01:33:43 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+   int n,i;
+   
+ printf("enter no of times of hello");
+ scanf("%d",&n);
+ 
+ for(i=1;i<=n;i++)
+ {
+  printf("hello");
+}
+ return 0;
+}
+         
